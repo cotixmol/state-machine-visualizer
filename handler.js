66516dbce961fd467e24d63dacdd0e5add841e80
Constancio Molinengo <constancio.molinengo@outlook.com>
2024-07-05 17:28:17 -0300

    Refactor patches to not be redundant and clean the code.

--- handler.js
+++ handler.js
@@ -1,12 +1,9 @@
-// Import the initializeGraph function from the graphInit module
 import { initializeGraph } from './modules/graphInit.js';
 
-// Initialize an empty array to hold the state machine data
 let stateMachineData = [];
 
-// Fetch JSON data from the specified URL
 fetch('/state-machine.json')
-    .then(response => response.json()) // Convert the response to JSON
+    .then(response => response.json())
     .then(data => {
         stateMachineData = data; // Store the fetched data in stateMachineData
         initializeGraph(stateMachineData); // Initialize the graph with the fetched data
--- modules/graphInit.js
+++ modules/graphInit.js
@@ -1,26 +1,20 @@
-// Import necessary functions from other modules
 import { createNodes, createLinks } from './nodeLink.js';
 import { layoutGraph } from './layout.js';
 import { addEventHandlers } from './eventHandlers.js';
 
-// Function to initialize the graph with given data
-export function initializeGraph(data) {
-    const namespace = joint.shapes; // Reference to JointJS shape namespace
-    const graph = new joint.dia.Graph({}, { cellNamespace: namespace }); // Create a new JointJS graph
+export const initializeGraph = (state_machine) => {
+    // Basic JointJS setup
+    const namespace = joint.shapes;
+    const graph = new joint.dia.Graph({}, { cellNamespace: namespace }); 
 
-    // Create a new paper (canvas) for the graph
     const paper = new joint.dia.Paper({
-        el: document.getElementById('paper'), // DOM element to attach the paper to
-        model: graph, // Associate the paper with the graph model
-        background: { color: '#DDDDDD' }, // Set the background color of the paper
-        cellViewNamespace: namespace, // Namespace for cell views
-        width: window.innerWidth, // Set paper width to the window width
-        height: window.innerHeight, // Set paper height to the window height
-        gridSize: 10 // Set the grid size for the paper
+        el: document.getElementById('paper'),
+        model: graph,
+        cellViewNamespace: namespace
     });
 
-    const nodes = createNodes(data, graph); // Create nodes from the data and add them to the graph
-    createLinks(data, nodes, graph); // Create links between nodes based on the data
+    const nodes = createNodes(state_machine, graph);
+    createLinks(state_machine, nodes, graph);
     layoutGraph(graph, paper); // Layout the graph using the layout function
     addEventHandlers(paper); // Add event handlers to the paper for interactive functionalities
 }
